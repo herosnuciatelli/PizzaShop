27ce25a0d6d46bdcb4994c1875469cb121f4fd8a
herosnuciatelli <herosnuciatelli@gmail.com>
2024-02-11 09:37:22 -0300

    find error

--- src/app/(app)/(dashboard)/page.tsx
+++ src/app/(app)/(dashboard)/page.tsx
@@ -1,13 +1,13 @@
 import { DollarSignIcon, UtensilsIcon } from 'lucide-react'
-import {
-  DashboardCard,
-  DashboardCardCurrency,
-  DashboardCardDescription,
-  DashboardCardTitle,
-  DashboardCardValue,
-} from '../../../components/DashboardCard'
-import LineChart from '../../../components/LineChart'
-import DonutChart from '../../../components/DonutChart'
+// import {
+//   DashboardCard,
+//   DashboardCardCurrency,
+//   DashboardCardDescription,
+//   DashboardCardTitle,
+//   DashboardCardValue,
+// } from '../../../components/DashboardCard'
+// import LineChart from '../../../components/LineChart'
+// import DonutChart from '../../../components/DonutChart'
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
         <h2 className="font-bold text-4xl">Dashboard</h2>
       </div>
       <div className="grid grid-cols-1 gap-8 lg:grid-cols-4 ">
-        <DashboardCard>
+        {/* <DashboardCard>
           <DashboardCardTitle>
             <h3>Recipe(month)</h3>
             <DollarSignIcon className="w-4 text-zinc-300" />
@@ -50,7 +50,7 @@ export default function Home() {
           </DashboardCardTitle>
           <DashboardCardValue>14</DashboardCardValue>
           <DashboardCardDescription>-30%</DashboardCardDescription>
-        </DashboardCard>
+        </DashboardCard> */}
       </div>
       <div className="flex flex-col py-10 gap-5 lg:flex-row h-full">
         <div className="border border-zinc-800 px-7 py-10 w-full flex flex-col gap-5 lg:w-3/5">
@@ -63,7 +63,7 @@ export default function Home() {
             </div>
             <div></div>
           </div>
-          <LineChart />
+          {/* <LineChart /> */}
         </div>
         <div className="border border-zinc-800 px-7 py-10 w-full flex flex-col gap-5 lg:w-2/5">
           <div>
@@ -71,7 +71,7 @@ export default function Home() {
               <h3>Popular products</h3>
             </div>
           </div>
-          <DonutChart />
+          {/* <DonutChart /> */}
         </div>
       </div>
     </main>
